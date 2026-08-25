dc69c303b78128830c27f52c124795b7803b3f02
Adarsh-Singh07 <adarsh2001gop@gmail.com>
2026-08-25 22:20:10 +0000

    fix: add missing migration for return policy and warranty info

---
+++ db/migrations/versions/20260825_2220_000000000001_add_return_policy_and_warranty.py
@@ -0,0 +1,28 @@
+"""add return_policy and warranty_info to products
+
+Revision ID: 000000000001
+Revises: 46a1e8a5a2f2
+Create Date: 2026-08-25 22:20:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = '000000000001'
+down_revision: Union[str, None] = '46a1e8a5a2f2'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('products', sa.Column('return_policy', sa.String(length=255), nullable=True))
+    op.add_column('products', sa.Column('warranty_info', sa.String(length=255), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('products', 'warranty_info')
+    op.drop_column('products', 'return_policy')
